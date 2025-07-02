702d9e855e8b6edab2382da531782eac9f1c496b
muhamademil <muhamademil76@gmail.com>
2025-07-03 04:42:08 +0700

    feat(paymentManual): merge feature/booking

--- apps/api/src/routers/payment.router.ts
+++ apps/api/src/routers/payment.router.ts
@@ -1,20 +1,42 @@
-// src/routers/room.router.ts
 import { Router } from 'express';
 import { PaymentController } from '@/controllers/payment.controller';
+import { MidtransController } from '@/controllers/midtrans.controller';
 
 export class PaymentRouter {
   private router = Router();
   private controller = new PaymentController();
+  private midtransController = new MidtransController();
 
   constructor() {
     this.setRoutes();
   }
 
   private setRoutes(): void {
-    this.router.post('/payments', this.controller.createPayment.bind(this.controller));
-    this.router.get('/payments', this.controller.getAllPayments.bind(this.controller));
-    this.router.get('/payments/:id', this.controller.getPaymentById.bind(this.controller));
-    this.router.delete('/payments/:id', this.controller.deletePayment.bind(this.controller));
+    this.router.post(
+      '/payments',
+      this.controller.createPayment.bind(this.controller),
+    );
+    this.router.get(
+      '/payments',
+      this.controller.getAllPayments.bind(this.controller),
+    );
+    this.router.get(
+      '/payments/:id',
+      this.controller.getPaymentById.bind(this.controller),
+    );
+    this.router.delete(
+      '/payments/:id',
+      this.controller.deletePayment.bind(this.controller),
+    );
+
+    this.router.post(
+      '/payments/snap',
+      this.midtransController.createSnapTransaction.bind(this.midtransController),
+    );
+    this.router.post(
+      '/payments/snap/:bookingId',
+      this.midtransController.getSnapTransaction.bind(this.midtransController),
+    );
   }
 
   public getRouter(): Router {
