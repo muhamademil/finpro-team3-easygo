c642ad2e27b868419f59754faa7555bd141e81e8
muhamademil <muhamademil76@gmail.com>
2025-07-11 01:02:44 +0700

    fix(font): correct relative font path for next/font/local

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,42 +8,42 @@ import { Footer } from '@/components/Layouts/Footer';
 const satoshi = localFont({
   src: [
     {
-      path: '/fonts/Satoshi/Satoshi-Regular.otf',
+      path: './fonts/Satoshi/Satoshi-Regular.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '/fonts/Satoshi/Satoshi-Italic.otf',
+      path: './fonts/Satoshi/Satoshi-Italic.otf',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '/fonts/Satoshi/Satoshi-Medium.otf',
+      path: './fonts/Satoshi/Satoshi-Medium.otf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '/fonts/Satoshi/Satoshi-MediumItalic.otf',
+      path: './fonts/Satoshi/Satoshi-MediumItalic.otf',
       weight: '500',
       style: 'italic',
     },
     {
-      path: '/fonts/Satoshi/Satoshi-Bold.otf',
+      path: './fonts/Satoshi/Satoshi-Bold.otf',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '/fonts/Satoshi/Satoshi-BoldItalic.otf',
+      path: './fonts/Satoshi/Satoshi-BoldItalic.otf',
       weight: '700',
       style: 'italic',
     },
     {
-      path: '/fonts/Satoshi/Satoshi-Black.otf',
+      path: './fonts/Satoshi/Satoshi-Black.otf',
       weight: '900',
       style: 'normal',
     },
     {
-      path: '/fonts/Satoshi/Satoshi-BlackItalic.otf',
+      path: './fonts/Satoshi/Satoshi-BlackItalic.otf',
       weight: '900',
       style: 'italic',
     },
@@ -55,22 +55,22 @@ const satoshi = localFont({
 const clashGrotesk = localFont({
   src: [
     {
-      path: '/fonts/ClashGrotesk/ClashGrotesk-Regular.otf',
+      path: './fonts/ClashGrotesk/ClashGrotesk-Regular.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '/fonts/ClashGrotesk/ClashGrotesk-Medium.otf',
+      path: './fonts/ClashGrotesk/ClashGrotesk-Medium.otf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '/fonts/ClashGrotesk/ClashGrotesk-Semibold.otf',
+      path: './fonts/ClashGrotesk/ClashGrotesk-Semibold.otf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '/fonts/ClashGrotesk/ClashGrotesk-Bold.otf',
+      path: './fonts/ClashGrotesk/ClashGrotesk-Bold.otf',
       weight: '700',
       style: 'normal',
     },
