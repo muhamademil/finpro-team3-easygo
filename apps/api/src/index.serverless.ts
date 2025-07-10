b9daaa03e98070677253c0a9864125c380bdbc71
muhamademil <muhamademil76@gmail.com>
2025-07-10 15:16:06 +0700

    fix: change api index serverless

--- apps/api/src/index.serverless.ts
+++ apps/api/src/index.serverless.ts
@@ -1,23 +1,15 @@
-// import { NowRequest, NowResponse } from '@vercel/node';
-// import { createServer } from 'http';
-// import { parse } from 'url';
-// import App from '../src/app';
-
-// const appInstance = new App();
-// const expressApp = appInstance.expressApp;
-
-// export default function handler(req: NowRequest, res: NowResponse) {
-//   const parsedUrl = parse(req.url ?? '/', true);
-//   req.url = parsedUrl.path || '/'
-
-//   const server = createServer((req2, res2) => {
-//     expressApp(req2, res2);
-//   });
-
-//   server.emit('request', req, res);
-// }
-import 'module-alias/register';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import App from './app';
 
 const app = new App();
-export default app.expressApp;
+const server = app.expressApp;
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return server(req, res);
+}
+
+// import 'module-alias/register';
+// import App from './app';
+
+// const app = new App();
+// export default app.expressApp;
