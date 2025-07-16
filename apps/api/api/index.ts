0b8a93b1f1e98e22bcf7528693e754821cab7c79
muhamademil <muhamademil76@gmail.com>
2025-07-16 09:04:42 +0700

    fix: manual tsconfig paths

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,6 +1,16 @@
-import 'tsconfig-paths/register';
+import { register } from 'tsconfig-paths';
+import { join } from 'path';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import App from '../src/app';
+
+// Manual config path
+const tsconfigPath = join(__dirname, '../tsconfig.json');
+register({
+  baseUrl: join(__dirname, '../'), // pointing to apps/api
+  paths: {
+    "@/*": ["src/*"]
+  }
+});
 
 const expressApp = new App().expressApp;
 
